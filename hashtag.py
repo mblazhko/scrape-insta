63b076380df4e7ac2aff8a7d250b08ed3f008d56
Acuta Pugione <88377885+Acutapugione@users.noreply.github.com>
2023-11-17 17:13:40 +0200

    Update hashtag.py

--- hashtag.py
+++ hashtag.py
@@ -5,11 +5,12 @@ def get_hashtag_info_with_provided_amount(
     cl: Client, tag_name: str, amount: int
 ):
     cl.delay_range = [1, 3]
-    hashtag = cl.hashtag_medias_top_v1(name=tag_name, amount=amount)
+    hashtags = cl.hashtag_medias_top_v1(name=tag_name, amount=amount)
 
     followers_list = []
 
-    for item.dict() in hashtag:
+    for hashtag in hashtags:
+        item = hashtag.dict()
         post_id = item["code"]
         user = item["user"]["username"]
         followers_count = cl.user_info_by_username_v1(user).dict()[
